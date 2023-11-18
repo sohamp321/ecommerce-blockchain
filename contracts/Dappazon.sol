25616abf2e74bdfff4a222c961ff4e83ef644283
sohamp321 <sohamp321@gmail.com>
2023-11-18 21:26:10 +0530

    Added Functionalities of Listing and Buying

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,8 +1,82 @@
 // SPDX-License-Identifier: UNLICENSED
+
+// * Do not use decimals for the price of smart contract... WE USE "WEI" ... LIKE PAISE for ETHEREUM
+
 pragma solidity ^0.8.9;
 
 contract Dappazon {
-    //Code goes here...abi
+    //creating owner as a state variable
+    address public owner;
 
-    string public name = "Dappazon";
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    struct Order{
+        uint256 time;
+        Item item;
+    }
+
+    //key => value(Item)
+    mapping(uint256 => Item) public items;
+
+    event List(string name, uint256 cost, uint256 capacity);
+
+    //Only to allow the vendor to use this function
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
+    }
+    
+    constructor() {
+        // name = "Dappazon";
+        owner = msg.sender;
+    }
+
+    //list products
+    function list(
+        uint256 _id, 
+        string memory _name, 
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+        ) public onlyOwner {
+            //Create item struct
+            Item memory item = Item(_id, _name, _category, _image, _cost, _rating, _stock);
+
+            //Save item struct to blockchain
+            items[_id] = item;
+
+            //Emit an event....subscribing to the event leads to notifications
+            //also lets you identify the number of times the fuction was called
+            emit List(_name, _cost, _stock);
+    }
+
+    //buy products
+    function buy(uint256 _id) public payable{
+        //Receive
+        //checked in Dappazon.js
+
+        //Fetch Item
+        Item memory item = items[_id];
+
+        //Create an Order
+        Order memory order = Order(block.timestamp, item);
+
+        //Subtract Stock
+
+        //Emit Event
+    }
+
+
+
+    //withdraw funds
 }
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,16 +1,123 @@
+/* eslint-disable no-undef */
 const { expect } = require("chai")
 
+//Gives the WEI value of the cost
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
+//Variables for testing the listing
+const ID = 1
+const NAME = "Shoes"
+const CATEGORY = "Accessories"
+//! Look at the tutorial to using IPFS
+const IMAGE = "Image Link from IPFS"
+const COST = tokens(1)
+const RATING = 5
+const STOCK = 1
+
 describe("Dappazon", () => {
+  
+  let dappazon
+  let deployer
+  let buyer
 
-  it('has a name', async () =>{
+
+  beforeEach(async () => {
+    //Setup Accounts
+    //! This is not working pls explain why
+    // console.log (await ether.getSigners())
+
+    [deployer, buyer] = await ethers.getSigners()
+    // console.log(deployer.address, buyer.address)
+
+  
+    //Check if the contract has been deployed
+    // eslint-disable-next-line no-undef
     const Dappazon = await ethers.getContractFactory('Dappazon')
-    const dappazon = await Dappazon.deploy()
+    dappazon = await Dappazon.deploy()
+  })
 
-    expect(await dappazon.name()).to.equal('Dappazon')
-  } )
+  describe("Deployment Functions:", () => {
+
+    it("Sets the owner", async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address)
+    })
+
+    // it('has a name', async () =>{
+    //   expect(await dappazon.name()).to.equal('Dappazon')
+    // } )
+  })
+
+
+  describe("Listing", () => {
+    let transaction 
+
+    beforeEach(async () => {
+      transaction = await dappazon.connect(deployer).list(
+        ID,
+        NAME,
+        CATEGORY,
+        IMAGE,
+        COST,
+        RATING,
+        STOCK
+      )
+
+      await transaction.wait()
+
+    })
+
+    it("Returns item attributes" , async() => {
+      const item = await dappazon.items(ID)
+
+      // console.log(item)
+
+      expect(item.id).to.equal(ID)
+      expect(item.name).to.equal(NAME)
+      expect(item.category).to.equal(CATEGORY)
+      expect(item.image).to.equal(IMAGE)
+      expect(item.cost).to.equal(COST)
+      expect(item.rating).to.equal(RATING)
+      expect(item.stock).to.equal(STOCK)
+    })
+
+    it("Emits an event", async () => {
+      expect(transaction).to.emit(dappazon, "List")
+    })
+  })
+
+  describe("Buying", () => {
+    let transaction 
+
+    beforeEach(async () => {
+      //List the item
+      transaction = await dappazon.connect(deployer).list(
+        ID,
+        NAME,
+        CATEGORY,
+        IMAGE,
+        COST,
+        RATING,
+        STOCK
+      )
+
+      await transaction.wait()
+
+      //{value:COST} because we used the modifier payable
+      transaction = await dappazon.connect(buyer).buy(ID, {value : COST})
+
+    })
+
+    it("Updates the Contract Balance" , async() => {
+      const result = await ethers.provider.getBalance(dappazon.address)
+      // console.log(result)
+      expect(result).to.equal(COST)
+    })
+
+    // it("Emits an event", async () => {
+    //   expect(transaction).to.emit(dappazon, "List")
+    // })
+  })
 
 })
